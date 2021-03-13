349d7086a5fbc4ff8486c0ec1c1f3cdf22f035e7
kain <kainpro@foxmail.com>
2021-03-13 10:39:04 +0800

    update

--- src/seed/permission.ts
+++
@@ -1,6 +0,0 @@
-import { getRepository } from 'typeorm';
-import { Permission } from '../entity/permission';
-
-export async function permission(vars: any) {
-  await getRepository(Permission).insert([]);
-}
